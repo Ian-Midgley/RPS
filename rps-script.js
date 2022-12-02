6c53003c166f4c90d60e82cad1732e672f71da35
Ian Midgley <80529147+Ian-Midgley@users.noreply.github.com>
2022-12-02 15:55:59 +0000

    create functions for player choice, comp choice, and to play a single round.

--- rps-script.js
+++ rps-script.js
@@ -1,5 +1,4 @@
 // ask user if they want to play
-confirm ("Would you like to play a game of Rock, Paper, Scissors?")
 if (confirm ("Would you like to play a game of Rock, Paper, Scissors?")) {
     alert ("Ok lets play a 5 round game");
 }
@@ -7,10 +6,44 @@ else {
     alert ("Well that's boring")
 }
 // ask user for there choice
+function playerChoice() {
+    let userSelection = prompt ("please enter your choice (Rock, Paper or Scissors)");
+    return userSelection.toLowerCase();
+}
+
 
 // create random choice for computer
+const element = ["rock" ,"paper","Scissors "];
+
+function compChoice() {
+    let compSelection = Math.floor(Math.random() * element.length);
+    return compSelection;
+}
 
 // play a round and give a winner 
+function round(playerMove, compMove) {
+    let result;
+
+    switch(true) {
+        case playerMove===compMove:
+            result="tie";
+            break;
+        case playerMove==="rock" && compMove==="scissors":
+        case playerMove==="paper" && compMove==="rock":
+        case playerMove==="scissors" && compMove==="paper":
+            result="win";
+            break;
+        case playerMove==="rock" && compMove==="paper":
+        case playerMove==="paper" && compMove==="scissors":
+        case playerMove==="scissors" && compMove==="rock":
+            result="loose";
+            break;
+        default:
+            result="OOPS"
+    }
+    return result;
+
+}
 
 // create a best of five game calling the round function 
 
