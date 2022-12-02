4e3472abf56de71a5c4314b9ab1af306794b3bc9
Ian Midgley <80529147+Ian-Midgley@users.noreply.github.com>
2022-12-02 13:51:50 +0000

    add comments/pseudocode to js file

---
+++ rps-script.js
@@ -0,0 +1,11 @@
+// ask user if they want to play
+
+// ask user for there choice
+
+// create random choice for computer
+
+// play a round and give a winner 
+
+// create a best of five game calling the round function 
+
+// show scores as the game is played then announce win or loss when the game is finished 
